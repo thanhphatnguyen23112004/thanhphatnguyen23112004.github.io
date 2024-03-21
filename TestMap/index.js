3a7c9c7c8e55300e89c981634e71800725472b76
thanhphatnguyen23112004 <thanhphatnguyen23112004@gmail.com>
2024-03-21 22:56:36 +0700

    change option code

--- TestMap/index.js
+++ TestMap/index.js
@@ -1,12 +1,17 @@
-let map;
+// Khởi tạo Google Maps API
+const map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 21.027763, lng: 105.834167}, // Tọa độ TP. Hồ Chí Minh
+    zoom: 12,
+});
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+// Thêm marker vào bản đồ
+const marker = new google.maps.Marker({
+    position: {lat: 21.027763, lng: 105.834167},
+    map: map,
+    title: "TP. Hồ Chí Minh",
+});
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-initMap();
+// Hiển thị thông tin khi click vào marker
+marker.addListener("click", function() {
+    alert("Bạn đang ở TP. Hồ Chí Minh!");
+});
