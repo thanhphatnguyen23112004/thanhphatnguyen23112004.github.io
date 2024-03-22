d7726cec03e9885daf1156be46c38ccc1dc2d07a
Đoàn Nguyễn Lâm <157136846+NguyLam2704@users.noreply.github.com>
2024-03-22 22:50:24 +0800

    fix vite.config and add plugin-react
    fix

--- js-samples/vite.config.js
+++ js-samples/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     hmr:
+//       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
+//         ? 443
+//         : undefined,
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    hmr:
-      process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
-        ? 443
-        : undefined,
-  },
-});
+  plugins: [react()],
+  base: "/"
+})
