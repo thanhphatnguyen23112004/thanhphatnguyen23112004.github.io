0b76abba3fb781f610434fb2da08c3f6bcee2dd3
Đoàn Nguyễn Lâm <157136846+NguyLam2704@users.noreply.github.com>
2024-03-23 12:32:39 +0800

    fix vite config
    fix

--- js-samples/vite.config.js
+++ js-samples/vite.config.js
@@ -10,31 +10,16 @@
 //   },
 // });
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/'
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  base: './', // Change the base configuration to an object
-});
+  }
+})
