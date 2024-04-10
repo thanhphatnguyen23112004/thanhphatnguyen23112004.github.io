541967e45b3cf1bded7703767a4f27b39b2d8046
thanhphatnguyen23112004 <thanhphatnguyen23112004@gmail.com>
2024-04-10 23:20:23 +0700

    fix logic

--- caesar.js
+++ caesar.js
@@ -1,45 +1,11 @@
-function btnEncrypt() {
-    document.getElementById("resultBruteforce").innerHTML = "";
-    var plainText = document.getElementById("plainText").value;
-    if (plainText.length == 0) {
-        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Plain text';
-        return;
-    }
-    var key = document.getElementById("key").value.toUpperCase();
-    if ((isNaN(key) && key.length > 1) || (!isNaN(key) && key.length > 2) || (key.length == 0)) {
-        document.getElementById("errorMsg").innerHTML = 'Khóa K bị rỗng hoặc sai!';
-        return;
-    }
-    if (!isKeyValid(key)) {
-        document.getElementById("errorMsg").innerHTML = 'Khóa K không hợp lệ.';
-        return;
-    }
-    if (!isNaN(key)) {
-        if ((parseInt(key) < 0) || (parseInt(key) > 26)) {
-            document.getElementById("errorMsg").innerHTML = 'Khóa K chỉ từ 0 đến 26';
-            return;
-        }
-        key = String.fromCharCode(parseInt(key) + 65);
-    }
-    var cipherText = crypt(plainText, key);
-    document.getElementById("cipherText").value = cipherText;
-    document.getElementById("errorMsg").innerHTML = "";
-}
-
-function crypt(text, key, isDecrypt = false) {
-    var codeKey = key.charCodeAt() - 65;
-    if (isDecrypt == true) {
-        codeKey = 26 - codeKey;
-    }
-    var result = "";
+function encrypt(text, key) {
+    let result = '';
     for (let i = 0; i < text.length; i++) {
-        var codeTxt = text.charCodeAt(i);
-        if (codeTxt >= 65 && codeTxt <= 90) {
-            let resultCode = ((codeTxt - 65) + codeKey) % 26;
-            result += String.fromCharCode(resultCode + 65);
-        } else if (codeTxt >= 97 && codeTxt <= 122) {
-            let resultCode = ((codeTxt - 97) + codeKey) % 26;
-            result += String.fromCharCode(resultCode + 97);
+        let charCode = text.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            result += String.fromCharCode((charCode - 65 + key) % 26 + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            result += String.fromCharCode((charCode - 97 + key) % 26 + 97);
         } else {
             result += text.charAt(i);
         }
@@ -47,41 +13,20 @@ function crypt(text, key, isDecrypt = false) {
     return result;
 }
 
-function btnDecrypt() {
-    document.getElementById("resultBruteforce").innerHTML = "";
-    var cipherText = document.getElementById("cipherText").value;
-    if (cipherText.length == 0) {
-        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Cipher text';
-        return;
-    }
-    document.getElementById("cipherText").value = cipherText;
-    var key = document.getElementById("key").value.toUpperCase();
-    if (key.length > 1) {
-        document.getElementById("errorMsg").innerHTML = 'Khóa K chỉ có thể chứa một ký tự';
-        return;
-    } else if (key.length == 0) {
-        document.getElementById("plainText").value = "";
-        var result = "<b>Tổng hợp các trường hợp phá mã:</b><br/><table><tr><th><b>Key K</b></th><th><b>PlainText</b></th></tr>";
-        for (let i = 65; i <= 90; i++) {
-            result += "<tr><td>" + String.fromCharCode(i) + "</td><td>" + crypt(cipherText, String.fromCharCode(i), true) + "</td></tr>";
-        }
-        result += "</table>";
-        document.getElementById("resultBruteforce").innerHTML = result;
-    } else {
-        document.getElementById("key").value = key;
-        var plainText = crypt(cipherText, key, true);
-        document.getElementById("plainText").value = plainText;
-    }
-
-    document.getElementById("errorMsg").innerHTML = "";
+function decrypt(text, key) {
+    return encrypt(text, 26 - key);
 }
 
-function isKeyValid(key) {
-    for (let i=0; i < key.length; i++) {
-        codeKey = key.charCodeAt(i);
-        if (!((codeKey >= 65 && codeKey <= 90) || (codeKey >= 48 && codeKey <= 57))) {
-            return false;
-        }
-    }
-    return true;
+function btnEncrypt() {
+    let plaintext = document.getElementById('plainText').value;
+    let key = parseInt(document.getElementById('key').value) % 26;
+    let ciphertext = encrypt(plaintext, key);
+    document.getElementById('cipherText').value = ciphertext;
+}
+
+function btnDecrypt() {
+    let ciphertext = document.getElementById('cipherText').value;
+    let key = parseInt(document.getElementById('key').value) % 26;
+    let plaintext = decrypt(ciphertext, key);
+    document.getElementById('plainText').value = plaintext;
 }
--- playfair.js
+++ playfair.js
@@ -1,78 +1,63 @@
 function updateMatrixKey() {
-    var a = "abc";
     var alph = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-    var key = document.getElementById("key").value.toUpperCase().replace(/[^A-Z_]+/g, "");
+    var key = document.getElementById("key").value.toUpperCase().replace(/[^A-Z]+/g, "");
     key = key.replace('J', 'I');
     document.getElementById("key").value = key;
     key = removeDuplicate(key + alph);
-    for (let i=0; i < 25; i++) {
+    for (let i = 0; i < 25; i++) {
         document.getElementById(getRow(i).toString() + getCol(i).toString()).innerHTML = key.charAt(i);
     }
 }
 
-
 function crypt(message, matrixKey, isDecrypt = false) {
-    var text = message.replace("J", "I");
-    text = text.replace("j", "i");
+    var text = message.toUpperCase().replace(/[^A-Z]+/g, "").replace('J', 'I');
     var result = "";
-    var map = [];
-    var k = 1; //1 => encrypt, -1 => decrypt
-    if (isDecrypt == true) {
-        k = 4;
+    var k = isDecrypt ? 4 : 1; // 1 => encrypt, 4 => decrypt
+
+    // Chèn "X" và xử lý các cặp ký tự giống nhau
+    for (let i = 0; i < text.length; i += 2) {
+        let chrA = text.charAt(i);
+        let chrB = (i + 1 < text.length) ? text.charAt(i + 1) : 'X'; // Chèn 'X' nếu là ký tự cuối cùng và không tạo thành cặp
+
+        if (chrA == chrB) {
+            chrB = 'X';
+            text = text.substr(0, i + 1) + chrB + text.substr(i + 1);
+        }
+
+        result += encodePair(chrA, chrB, matrixKey, k);
     }
-    for (let i=0; i < text.length; i++) {
-        let codeTxt = text.charCodeAt(i);
-        if ((codeTxt >= 65 && codeTxt <= 90) || (codeTxt >= 97 && codeTxt <= 122)) {
-            map.push(i);
-        }
+    return result;
+}
+
+function encodePair(chrA, chrB, matrixKey, k) {
+    let posA = matrixKey.indexOf(chrA);
+    let posB = matrixKey.indexOf(chrB);
+
+    let rowA = getRow(posA);
+    let colA = getCol(posA);
+    let rowB = getRow(posB);
+    let colB = getCol(posB);
+
+    let encA, encB;
+
+    if (rowA === rowB) {
+        encA = matrixKey.charAt(rowA * 5 + (colA + k) % 5);
+        encB = matrixKey.charAt(rowB * 5 + (colB + k) % 5);
+    } else if (colA === colB) {
+        encA = matrixKey.charAt(((rowA + k) % 5) * 5 + colA);
+        encB = matrixKey.charAt(((rowB + k) % 5) * 5 + colB);
+    } else {
+        encA = matrixKey.charAt(rowA * 5 + colB);
+        encB = matrixKey.charAt(rowB * 5 + colA);
     }
-    if ((map.length % 2) == 1) {
-        text += "X";
-        map.push(text.length - 1);
-    }
 
-    for (let i=0; i < map.length; i+=2) {
-        let txtA = text.charAt(map[i]);
-        let codeTxtA = txtA.charCodeAt();
-        let txtB = text.charAt(map[i+1]);
-        let codeTxtB = txtB.charCodeAt();
-
-        if (txtA == txtB) {
-            txtB = "X";
-        }
-
-        let posA = matrixKey.indexOf(txtA.toUpperCase());
-        let posB = matrixKey.indexOf(txtB.toUpperCase());
-        let chrCphA = "";
-        let chrChpB = "";
-
-        if(getRow(posA) == getRow(posB)) {
-            chrCphA = matrixKey.charAt((getRow(posA) * 5) + ((getCol(posA) + k) % 5));
-            chrCphB = matrixKey.charAt((getRow(posB) * 5) + ((getCol(posB) + k) % 5));
-        } else if(getCol(posA) == getCol(posB)) {
-            chrCphA = matrixKey.charAt((((getRow(posA) + k) % 5) * 5) + (getCol(posA) % 5));
-            chrCphB = matrixKey.charAt((((getRow(posB) + k) % 5) * 5) + (getCol(posB) % 5));
-        } else {
-            chrCphA = matrixKey.charAt((getRow(posA) * 5) + getCol(posB));
-            chrCphB = matrixKey.charAt((getRow(posB) * 5) + getCol(posA));
-        }
-
-        if(isLowerCase(codeTxtA)) {
-            chrCphA = chrCphA.toLowerCase();
-        }
-        if(isLowerCase(codeTxtB)) {
-            chrCphB = chrCphB.toLowerCase();
-        }
-        text = text.setCharAt(map[i], chrCphA);
-        text = text.setCharAt(map[i+1], chrCphB);
-    }
-    return text;
+    return encA + encB;
 }
 
 function btnEncrypt() {
     var plainText = document.getElementById("plainText").value;
     if (plainText.length == 0) {
-        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Plain text kìa! :D';
+        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Plain text!';
         return;
     }
     var matrixKey = getMatrixKey();
@@ -80,7 +65,7 @@ function btnEncrypt() {
         document.getElementById("errorMsg").innerHTML = 'Khóa K không được để trống';
         return;
     }
-    
+
     var cipherText = crypt(plainText, matrixKey);
     document.getElementById("cipherText").value = cipherText;
     document.getElementById("errorMsg").innerHTML = "";
@@ -89,19 +74,18 @@ function btnEncrypt() {
 function btnDecrypt() {
     var cipherText = document.getElementById("cipherText").value;
     if (cipherText.length == 0) {
-        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Cipher text kìa!';
+        document.getElementById("errorMsg").innerHTML = 'Chưa nhập Cipher text!';
         return;
     }
     document.getElementById("cipherText").value = cipherText;
     var matrixKey = getMatrixKey();
     if (matrixKey.length == 0) {
-        document.getElementById("errorMsg").innerHTML = 'Khóa K không được để trống nhé!';
+        document.getElementById("errorMsg").innerHTML = 'Khóa K không được để trống!';
         return;
     }
-    
+
     var plainText = crypt(cipherText, matrixKey, true);
     document.getElementById("plainText").value = plainText;
-
     document.getElementById("errorMsg").innerHTML = "";
 }
 
@@ -110,13 +94,13 @@ function getCol(i) {
 }
 
 function getRow(i) {
-    return parseInt(i/5);
+    return Math.floor(i / 5);
 }
 
 function getMatrixKey() {
     var result = "";
-    for (let i=0; i < 5; i++) {
-        for (let j=0; j < 5; j++) {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
             result += document.getElementById(i.toString() + j.toString()).innerHTML;
         }
     }
@@ -131,18 +115,4 @@ function removeDuplicate(text) {
         }
     }
     return result;
-}
-
-function isLowerCase(code) {
-    if (code >= 97 && code <= 122) {
-        return true;
-    }
-    return false;
-}
-
-String.prototype.setCharAt = function (index, chr) {
-    if(index < this.length) {
-        return this.substr(0, index) + chr + this.substr(index + 1);
-    }
-    return this;
 }
